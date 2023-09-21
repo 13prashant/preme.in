fe2d01b2a90a099b848b354cdc8778125a84c523
13prashant <pacific1392@gmail.com>
2023-09-21 19:22:31 +0530

    added static instagram posts

--- src/components/home/InstagramFeed.tsx
+++ src/components/home/InstagramFeed.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
+import { instagramPosts } from "@/utils/constants";
 
-interface InstagramPost {
+export interface InstagramPost {
   caption?: string;
-  id: string;
+  id: string | number;
   media_type: "IMAGE" | "VIDEO";
   media_url: string;
   permalink: string;
   thumbnail_url?: string;
-  timestamp: Date;
+  timestamp?: Date;
 }
 
 const getInstagramPosts = async (): Promise<InstagramPost[]> => {
   const res = await fetch(
-    `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,permalink,thumbnail_url,timestamp&limit=10&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_TOKEN}`
+    `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,permalink,thumbnail_url,timestamp&limit=9&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_TOKEN}`
   );
 
   const data = await res.json();
@@ -21,8 +22,8 @@ const getInstagramPosts = async (): Promise<InstagramPost[]> => {
   return data.data;
 };
 
-export default async function InstagramFeed() {
-  const instagramPosts = await getInstagramPosts();
+export default async function InstagramPosts() {
+  // const instagramPosts = await getInstagramPosts();
 
   return (
     <section className="preme-container duration-300">
@@ -33,26 +34,28 @@ export default async function InstagramFeed() {
       <div className="grid place-items-center">
         <div className="max-w-fit grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1 duration-300">
           {instagramPosts.map(
-            ({ id, media_type, thumbnail_url, permalink, media_url }) => {
-              if (media_type === "IMAGE" && media_url) {
-                return (
-                  <Link key={id} href={permalink} target="_blank">
-                    <div className="relative group">
-                      <div className="relative h-72 w-72 duration-300">
-                        <Image
-                          style={{ objectFit: "cover" }}
-                          src={media_url}
-                          fill
-                          alt={media_url}
-                        />
-                      </div>
-                      <div className="bg-black/30 absolute left-0 top-0 h-full w-full hidden group-hover:grid group-hover-text place-items-center text-white duration-300">
-                        View on Instagram
-                      </div>
+            ({ id, permalink, media_url, thumbnail_url, media_type }) => {
+              return (
+                <Link key={id} href={permalink} target="_blank">
+                  <div className="relative group">
+                    <div className="relative h-72 w-72 duration-300">
+                      <Image
+                        style={{ objectFit: "cover" }}
+                        src={
+                          media_type === "VIDEO"
+                            ? thumbnail_url || media_url
+                            : media_url
+                        }
+                        fill
+                        alt={media_url}
+                      />
                     </div>
-                  </Link>
-                );
-              }
+                    <div className="bg-black/30 absolute left-0 top-0 h-full w-full hidden group-hover:grid group-hover-text place-items-center text-white duration-300">
+                      View on Instagram
+                    </div>
+                  </div>
+                </Link>
+              );
             }
           )}
         </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import { InstagramPost } from "@/components/home/InstagramFeed";
+
 export const INSTAGRAM_URL = "https://www.instagram.com/preme_qahanni/";
 export const FACEBOOK_URL = "https://www.facebook.com/prem.dave.33";
 export const TWITTER_URL = "https://twitter.com/premdave99";
@@ -52,5 +54,80 @@ export const blogs = [
     id: 1,
     url: "https://to-be-precise-poem-preme-qahanni.blogspot.com/",
     name: "To be Precise",
+  },
+];
+
+export const instagramPosts: InstagramPost[] = [
+  {
+    id: 1,
+    permalink:
+      "https://www.instagram.com/p/CtDX3knhU3s/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/351014973_1221299688553806_5799730688531212884_n.heic?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=106&_nc_ohc=aLLZvXfQiucAX9WUc8r&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzExNzQ0MDM0MTAyOTI0NDM5Ng%3D%3D.2-ccb7-5&oh=00_AfATVgXYz1kW66Y6TUTqauToWp8BPpJm6e-Ce9Q8KyQBVg&oe=6510D8BC&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 2,
+    permalink:
+      "https://www.instagram.com/p/CtQK2luK1vq/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/352593299_796073295456625_5149826139618760473_n.heic?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=101&_nc_ohc=fm-F68ywBPUAX-zDFSt&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzEyMTA0MjI3MzU4NzY0OTUxNA%3D%3D.2-ccb7-5&oh=00_AfDxLKfqFQwv6knEE5xIO9A0bS-aqP58l0zTfEwjI6QIbA&oe=6510DFF1&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 3,
+    permalink:
+      "https://www.instagram.com/reel/CtLD7cLh0wQ/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/352552379_804818227492115_5150139897882062603_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=uoc1PMelPzIAX98KqHb&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzExOTYwNDQ0NTczMTA0ODQ2NA%3D%3D.2-ccb7-5&oh=00_AfB8QNHOb_3jD0RipwyAH267xElM5xuJy_PESxuYOCn0ew&oe=65122533&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 4,
+    permalink:
+      "https://www.instagram.com/p/Cssez89B2Wh/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/348851148_2893173564151463_7455624945895204932_n.heic?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=CALu6ujQC-0AX-OTCCW&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzExMDk5Njk1NDE0MzIyMTE1Mw%3D%3D.2-ccb7-5&oh=00_AfAwGLc0H1ZSbS_Gk1WUzbiddWF9tafPxbOQyKGaonbaIw&oe=651096E8&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 5,
+    permalink:
+      "https://www.instagram.com/reel/CsnT8bvhdVb/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/349081910_4263950107079025_4252563489095351656_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=Uwg_JumN3HEAX9S3t2X&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzEwOTU0MTc4MzU2MzI2MTI3NQ%3D%3D.2-ccb7-5&oh=00_AfDDV1ZCpGm4VVl-I147Fn-ewHr4e1CVgqjaPhodI9wKTA&oe=6511012A&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 6,
+    permalink:
+      "https://www.instagram.com/reel/CsaZch7BMyW/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/347624472_627009252373196_1997684713777252076_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=woP09HSQk4wAX8a_Qce&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzEwNTkwNjgwNDc1NzE1NDk2Ng%3D%3D.2-ccb7-5&oh=00_AfA-_KH2mqSLlCwq_HpWvMOOXGMMJ4sa7eWpezWOt5Ycpg&oe=6510ED93&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 7,
+    permalink:
+      "https://www.instagram.com/reel/CsIXvpyBXAo/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/346284918_935470517731796_6132653965745268155_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=8Rc1PsTK7PYAX_gOGhn&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzEwMDgzMjc3MzE5MjM4MDQ1Ng%3D%3D.2-ccb7-5&oh=00_AfB0yWf_J-oIqjeBo-8Cmmp2QyfQn-mQy0RqqrEhMvDa5Q&oe=651086E4&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 8,
+    permalink:
+      "https://www.instagram.com/p/CsAgzkyBZ_B/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/345888078_264982522594934_151550150763989103_n.heic?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=102&_nc_ohc=jY12chM26FAAX-Ipf2G&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzA5ODYyMDgyNTMwNjUwNTE1Mw%3D%3D.2-ccb7-5&oh=00_AfA1FUM_Vk1JgKEszK9HQ4-jk56PiF1qD8O_M4eczV747Q&oe=651087F3&_nc_sid=10d13b",
+    media_type: "IMAGE",
+  },
+  {
+    id: 9,
+    permalink:
+      "https://www.instagram.com/p/CZ4otovMuuV/?igshid=MzRlODBiNWFlZA==",
+    media_url:
+      "https://scontent.cdninstagram.com/v/t51.2885-15/273724639_247869487503107_7925582400438707559_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=H1Dw7t_GskkAX9BVnhD&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjc3MjE0NDYyODYyNDk3Njc4OQ%3D%3D.2-ccb7-5&oh=00_AfCjXvoBFCWfWoKSbH8iiJAspWFWePtCUPLpcwvHW_NSgA&oe=651086ED&_nc_sid=10d13b",
+    media_type: "IMAGE",
   },
 ];
