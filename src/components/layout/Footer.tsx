a7dac65ecad36694a8147714f15a5e35b4d73bf4
13prashant <pacific1392@gmail.com>
2023-09-21 17:48:24 +0530

    fixed footer affecting blogs page

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,38 +4,36 @@ import { DEVELOPER_WEBSITE_URL } from "@/utils/constants";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 ">
-      <div className="preme-container">
-        <div className="flex flex-col gap-2">
-          <Logo text="Prem D Dave" size="sm" />
-          <SocialLinks size={20} />
-        </div>
-        <hr className="my-3" />
-        <div className="flex flex-col gap-3 md:flex-row md:justify-between md:items-end text-gray-500 text-sm">
-          <div className="">
-            <p className="mb-1">
-              Copyright © {new Date().getFullYear()} Prem Dave. All rights
-              reserved.
-            </p>
-            <Link className="hover:text-yellow-400 duration-300" href="/">
-              Terms of Use
-            </Link>
-            <span> | </span>
-            <Link className="hover:text-yellow-400 duration-300" href="/">
-              Privacy Policy
-            </Link>
-          </div>
-          <p className="">
-            Made with <span className="text-yellow-400">&hearts;</span> by{" "}
-            <Link
-              className="underline hover:text-yellow-400 duration-300"
-              href={DEVELOPER_WEBSITE_URL}
-              target="_blank"
-            >
-              Prashant
-            </Link>
+    <footer className="preme-container">
+      <div className="flex flex-col gap-2">
+        <Logo text="Prem D Dave" size="sm" />
+        <SocialLinks size={20} />
+      </div>
+      <hr className="my-3" />
+      <div className="flex flex-col gap-3 md:flex-row md:justify-between md:items-end text-gray-500 text-sm">
+        <div className="">
+          <p className="mb-1">
+            Copyright © {new Date().getFullYear()} Prem Dave. All rights
+            reserved.
           </p>
+          <Link className="hover:text-yellow-400 duration-300" href="/">
+            Terms of Use
+          </Link>
+          <span> | </span>
+          <Link className="hover:text-yellow-400 duration-300" href="/">
+            Privacy Policy
+          </Link>
         </div>
+        <p className="">
+          Made with <span className="text-yellow-400">&hearts;</span> by{" "}
+          <Link
+            className="underline hover:text-yellow-400 duration-300"
+            href={DEVELOPER_WEBSITE_URL}
+            target="_blank"
+          >
+            Prashant
+          </Link>
+        </p>
       </div>
     </footer>
   );
